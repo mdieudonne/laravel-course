35782dc38dc133b8cc1da5a2fe654b09b7970ea2
mdieudonne <michael.dieudonne@outlook.com>
2021-02-12 16:22:37 +0100

    feat: add ContactsCreate

---
+++ resources/js/components/InputField.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="relative pb-4">
+        <label :for="name" class="text-blue-500 pt-2 uppercase text-xs font-bold absolute">{{ label }}</label>
+        <input v-model="value" :id=name type="text"
+               class="pt-8 w-full text-gray-900 border-b pb-2 focus:outline-none focus:border-blue-400"
+               :placeholder="placeholder"
+               @input="updateField()">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "InputField",
+    props: [
+        'name',
+        'label',
+        'placeholder'
+    ],
+    data: () => {
+        return {
+            value: ''
+        }
+    },
+    methods: {
+        updateField() {
+            this.$emit('update:field', this.value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/views/ContactsCreate.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <form>
+            <input-field name="name" label="Contact Name" placeholder="Contact Name"
+                         @update:field="form.name = $event"/>
+            <input-field name="email" label="Contact Email" placeholder="Contact Ema"
+                         @update:field="form.email = $event"/>
+            <input-field name="company" label="Company" placeholder="Company" @update:field="form.company = $event"/>
+            <input-field name="birthday" label="Birthday" placeholder="MM/DD/YYYY"
+                         @update:field="form.birthday = $event"/>
+
+            <div class="flex justify-end">
+                <button class="py-2 px-4 text-red-700 rounded border mr-5 hover:border-red-700">Cancel</button>
+                <button class="bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-400">Add New Contact</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import InputField from "../components/InputField";
+
+export default {
+    name: "ContactsCreate",
+    components: {InputField},
+    data: () => {
+        return {
+            form: {
+                name: '',
+                email: '',
+                company: '',
+                birthday: '',
+            }
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
